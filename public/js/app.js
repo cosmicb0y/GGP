a10dda9a0381c9d7860597755e3a4297bb85d006
Hyowon Jang <jang.hw73@gmail.com>
2017-07-05 10:55:52 +0900

    Add button text change event

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,8 @@
 $(function(){
     $('.signup').click(function() {
         $('.footer').animate({
-            top: '200%'
-        });
+            'margin-top': '50%'
+        }, 1000);
+        $('.signup').html('이제, 시작해보죠');
     });
 });
