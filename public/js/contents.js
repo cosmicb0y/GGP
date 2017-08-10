fc35bb8dc00770a65f11b3bc94c7a6f7509be490
Hyowon Jang <jang.hw73@gmail.com>
2017-08-10 20:04:01 +0900

    Implement infinite scroll contents

--- public/js/contents.js
+++ public/js/contents.js
@@ -60,3 +60,53 @@ $(document).ready(function () {
     });
 
 });
+
+var page = 0;
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        ++page;
+        var url = "http://chacham.xyz:3000/api/contents/"+page;
+        //Change page later!!! Wooooooork!!!
+
+        $.getJSON(url, function(data) {
+            var items = [];
+            if( ! data.success){
+                //Get Error??
+            }
+            $.each( data.projects, function( i, data ) {
+                var thumbnail = data.thumbnail;
+                var category = data.category;
+                var writer = data.writer;
+                var name = data.name;
+                var summary = data.summary;
+                var likeCount = data.likeCount;
+                var view = data.viewed; //temporary puts 0, it should modify later.
+                var comments = data.commentCount;
+
+                $('.card-deck').append(
+                    '                <div class="card">\n' +
+                    "                    <div class=\"center-cropped\">\n" +
+                    '                        <img class="card-img-top" src="http://chacham.xyz:3000/'+thumbnail+' " alt="Card image cap">\n' +
+                    "                    </div>\n" +
+                    "                    <div class=\"card-block\">\n" +
+                    "                        <div class=\"row\" id=\"upperRow\">\n" +
+                    '                            <div class="col-6" id="category">'+category+'</div>\n' +
+                    '                            <div class="col-6" id="writer">'+writer+'</div>\n' +
+                    "                        </div>\n" +
+                    '                        <h4 class="card-title" id="name">'+name+'</h4>\n' +
+                    '                        <p class="card-text" id="summary">'+summary+'</p>\n' +
+                    "                        <div class=\"row\" id=\"lowerRow\">\n" +
+                    '                            <div class="col-4" id="views">'+view+'</div>\n' +
+                    '                            <div class="col-4" id="likes">'+likeCount+'</div>\n' +
+                    '                            <div class="col-4" id="comments">'+comments+'</div>\n' +
+                    "                        </div>\n" +
+                    "                    </div>\n" +
+                    "\n" +
+                    "                </div>")
+                    .children().last().on( 'click', ()=>{window.location.href = '/api/project/' + data.id} );;
+
+            });
+        });
+    }
+});
